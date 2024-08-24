a82a89a4c967c6d7936d0f0e1d7fa986026ab339
StefanoWork <stefanom87.work@outlook.it>
2024-08-24 18:36:18 +0200

    Add key

--- src/components/ListArticles/ListArticles.jsx
+++ src/components/ListArticles/ListArticles.jsx
@@ -37,7 +37,7 @@ export default function ListArticles({articles}) {
 
                         return (
                             <Link href={article.url} key={index}>
-                                <div className='sm:w-[75%]' >
+                                <div className='sm:w-[75%]' key={index} >
                                     <div className="sm:mb-6 mb-2 flex">
                                         <p className='hidden sm:flex w-[18%] text-sm text-gray-500 dark:text-white'>{formattedDate}</p>                       
 
@@ -77,31 +77,31 @@ export default function ListArticles({articles}) {
 
                     return (
                         <Link href={article.url} key={index}>
-                            <div className='w-[75%]' >
-                            <div className=" mb-6 flex" >
-                                <div className='w-[12%]'>                        
-                                <p className='text-sm text-gray-500 dark:text-white'>{formattedDate}</p>
-                                </div>
+                            <div className='w-[75%]' key={index} >
+                                <div className=" mb-6 flex" >
+                                    <div className='w-[12%]'>                        
+                                    <p className='text-sm text-gray-500 dark:text-white'>{formattedDate}</p>
+                                    </div>
 
-                                <div className='ml-2 w-[60%]'>
-                                    <p className='text-sm'>{noBy}</p>
-                                    <h2 className="font-bold text-xl mt-1 hover:underline">{article.title}</h2>
-                                    <p className="text-sm mt-4">{article.abstract}</p>
-                                    <p className='text-gray-500 dark:text-white text-sm mt-2'>{article.byline}</p>                      
-                                        
-                                </div>
+                                    <div className='ml-2 w-[60%]'>
+                                        <p className='text-sm'>{noBy}</p>
+                                        <h2 className="font-bold text-xl mt-1 hover:underline">{article.title}</h2>
+                                        <p className="text-sm mt-4">{article.abstract}</p>
+                                        <p className='text-gray-500 dark:text-white text-sm mt-2'>{article.byline}</p>                      
+                                            
+                                    </div>
 
-                                    {article.multimedia != null && (
-                                        <Image 
-                                            className="ml-4 object-contain" 
-                                            width={200} 
-                                            height={250}
-                                            src={article?.multimedia[1]?.url} 
-                                            alt={article?.multimedia[0]?.caption}
-                                        />
-                                    )}                               
-                            </div>
-                            <div className='h-[1px] bg-gray-500 opacity-75 my-4'></div>
+                                        {article.multimedia != null && (
+                                            <Image 
+                                                className="ml-4 object-contain" 
+                                                width={200} 
+                                                height={250}
+                                                src={article?.multimedia[1]?.url} 
+                                                alt={article?.multimedia[0]?.caption}
+                                            />
+                                        )}                               
+                                </div>
+                                <div className='h-[1px] bg-gray-500 opacity-75 my-4'></div>
 
                             </div>
                         </Link>
