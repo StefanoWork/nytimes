8f6435649719235791ad233826d752f0998c263e
StefanoWork <stefanom87.work@outlook.it>
2024-08-25 18:31:00 +0200

    Add link in page section

--- src/components/SectionBig/Section.jsx
+++ src/components/SectionBig/Section.jsx
@@ -9,16 +9,18 @@ const topArticles = articles.slice(0, 4);
         <>
             <div className="sm:flex mt-8">
                 <div className="sm:w-[66%] w-full mb-3 sm:mb-0">
-                    {topArticles[0]?.multimedia != null && (
-                    <Image 
-                        className="w-full object-fit"
-                        src={topArticles[0]?.multimedia[0]?.url} 
-                        alt={topArticles[0]?.multimedia[0]?.caption} 
-                        height={200}
-                        width={500}
-                    />)}
-                    <h2 className="font-bold text-xl sm:mt-9 mt-3 leading-tight sm:text-center dm">{topArticles[0]?.title}</h2>
-                    <p className="text-sm mt-2 sm:text-center sm:px-10">{topArticles[0]?.abstract}</p>
+                    <a href={topArticles[0]?.url}>
+                        {topArticles[0]?.multimedia != null && (
+                        <Image 
+                            className="w-full object-fit"
+                            src={topArticles[0]?.multimedia[0]?.url} 
+                            alt={topArticles[0]?.multimedia[0]?.caption} 
+                            height={200}
+                            width={500}
+                        />)}
+                        <h2 className="font-bold text-xl sm:mt-9 mt-3 leading-tight sm:text-center dm">{topArticles[0]?.title}</h2>
+                        <p className="text-sm mt-2 sm:text-center sm:px-10">{topArticles[0]?.abstract}</p>
+                    </a>
                     <div className="block sm:hidden h-[1px] bg-gray-600 opacity-25 mt-2"></div>
 
                 </div>
@@ -31,32 +33,34 @@ const topArticles = articles.slice(0, 4);
                         const formattedDate = format(new Date(article.published_date), 'MMM, dd, yyyy');
                         return (
                             <div className='mb-6' key={index}>
-                                <div className='sm:flex'>
-                                    <h2 className="hidden sm:block font-bold text-xl lg:mt-2 sm:w-[75%] dm">{article.title}</h2>
-                                    {article?.multimedia != null && (
-                                        <>
-                                            <Image 
-                                                className="hidden sm:block sm:ml-4 object-contain lg:w-[25%] w-[40%] self-start" 
-                                                width={150} 
-                                                height={150}
-                                                src={article?.multimedia[2]?.url} 
-                                                alt={article?.multimedia[0]?.caption} 
-                                            />
-                                            <Image 
-                                                className="block sm:hidden object-contain w-full" 
-                                                width={150} 
-                                                height={150}
-                                                src={article?.multimedia[1]?.url} 
-                                                alt={article?.multimedia[0]?.caption} 
-                                            />
-                                        </>
-                                    )}
-                                    <h2 className="block sm:hidden font-bold text-xl mt-2 sm:w-[75%] dm">{article.title}</h2>                                                                  
-                                </div>
-                                <p className="hidden lg:block text-sm sm:mt-4 mt-2">{article.abstract}</p>
-                                <p className='block lg:hidden text-[10px] mt-2 text-gray-300 dark:text-white'>{formattedDate}</p>
-                                <div className=" h-[1px] bg-gray-600 opacity-25 mt-2"></div>
-
+                                <a href={article.url}>
+                                    <div className='sm:flex'>
+                                        
+                                        <h2 className="hidden sm:block font-bold text-xl lg:mt-2 sm:w-[75%] dm">{article.title}</h2>
+                                        {article?.multimedia != null && (
+                                            <>
+                                                <Image 
+                                                    className="hidden sm:block sm:ml-4 object-contain lg:w-[25%] w-[40%] self-start" 
+                                                    width={150} 
+                                                    height={150}
+                                                    src={article?.multimedia[2]?.url} 
+                                                    alt={article?.multimedia[0]?.caption} 
+                                                />
+                                                <Image 
+                                                    className="block sm:hidden object-contain w-full" 
+                                                    width={150} 
+                                                    height={150}
+                                                    src={article?.multimedia[1]?.url} 
+                                                    alt={article?.multimedia[0]?.caption} 
+                                                />
+                                            </>
+                                        )}
+                                        <h2 className="block sm:hidden font-bold text-xl mt-2 sm:w-[75%] dm">{article.title}</h2>                                                                  
+                                    </div>
+                                    <p className="hidden lg:block text-sm sm:mt-4 mt-2">{article.abstract}</p>
+                                    <p className='block lg:hidden text-[10px] mt-2 text-gray-300 dark:text-white'>{formattedDate}</p>
+                                    <div className=" h-[1px] bg-gray-600 opacity-25 mt-2"></div>
+                                </a> 
                             </div>
                         )
                     })}
